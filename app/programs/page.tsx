08c56266651e79f8af5cd8bcd7d6be7f5ec285f3
James Randolph <james.randolph2016@gmail.com>
2024-08-07 17:43:35 -0400

    add folders and pages

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between lg:flex">
-          This is the Home Page
+          This is the programs parent page
       </div>
     </main>
   );
--- app/programs/regional-textile-economies.tsx
+++ app/programs/regional-textile-economies.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
-export default function Home() {
+export default function Regional() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between lg:flex">
-          This is the Home Page
+          This is the Regional Textile Page
       </div>
     </main>
   );
